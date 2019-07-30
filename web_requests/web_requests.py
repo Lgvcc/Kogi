ba6f9e3f5b0dc6a4312c7dcf1dfe429a11198228
mingyang.liang <18790334713@163.com>
2019-07-30 14:52:39 +0800

    爬虫基本功能完成

--- web_requests/web_requests.py
+++ web_requests/web_requests.py
@@ -6,6 +6,11 @@
 # @File : web_requests.py
 # @Software: PyCharm
 import requests
+import traceback
+
+from libs.logs import get_logger
+
+logger = get_logger('web_requests')
 
 
 class WebRequests(object):
@@ -21,18 +26,21 @@ class WebRequests(object):
             timeout = 120
         headers = kwargs.get('headers')
         fail = 0
+        res = None
         while fail < 5:
             try:
                 res = self.session.request(method_name, url, **kwargs)
-                print(res.status_code, '---')
+                # print(res.status_code, '---')
                 if res.status_code == requests.codes.ok:
+                    logger.info('*' * 20 + '请求成功: status_code: %s' % res.status_code + '*' * 20)
                     break
                 else:
                     raise requests.RequestException
             except Exception as e:
-                print('网络异常: ')
-
-
+                logger.info('*' * 20 + '网络异常' + '*' * 20)
+                err = traceback.format_exc()
+                logger.info(err)
+        return res
 
     def get(self, url, **kwargs):
         return self._requests_helper('GET', url, **kwargs)
